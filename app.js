03d53c57ff3ef5aebd1303ac32cdef55776c8a33
James Dansie <jdansie8336@gmail.com>
2019-07-12 16:52:39 -0700

    all the things are done! :D

--- app.js
+++ app.js
@@ -142,10 +142,20 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  //in the for loop can only update 1, need to prime the pump 
+  var prod = multiply(dynamicArray[0], dynamicArray[1])[0];
 
+  for(var index = 2; index < dynamicArray.length; index++){
+    //debug code
+    //console.log(prod, dynamicArray[index], index);
+
+    //same as total *= total[i];
+    prod = multiply(prod, dynamicArray[index])[0];
+  }
+  return [prod, 'The numbers ' + dynamicArray + ' have a product of ' + prod + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
